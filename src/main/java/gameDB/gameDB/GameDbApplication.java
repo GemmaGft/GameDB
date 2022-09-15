2bf401431123c7f026a4d6eaee38dc99550c4461
GemmaGft <gamr@gft.com>
2022-09-15 11:28:21 +0000

    initial commit

---
+++ src/main/java/gameDB/gameDB/GameDbApplication.java
@@ -0,0 +1,13 @@
+package gameDB.gameDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameDbApplication.class, args);
+	}
+
+}
