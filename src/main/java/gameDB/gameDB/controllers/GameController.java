e78dca480e63fd23732b06a6db0bc7bcaecc7637
oriolElias <Oriol.Elias@gft.com>
2022-09-15 15:22:56 +0000

    add some get methods

--- src/main/java/gameDB/gameDB/controllers/GameController.java
+++ src/main/java/gameDB/gameDB/controllers/GameController.java
@@ -1,4 +1,30 @@
 package gameDB.gameDB.controllers;
 
+import gameDB.gameDB.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
 public class GameController {
+
+    @Autowired
+    GameService gameService;
+
+    @GetMapping("/v1/games")
+    public HashMap<String, Object> getAllGenres() {
+        HashMap<String, Object> games = gameService.getAllGames();
+
+        return games;
+    }
+
+    @GetMapping("/v1/games/{game_pk}/additions")
+    public HashMap<String,Object> getListOfDlcForGame(@PathVariable int game_pk){
+        HashMap<String, Object> dlcs = gameService.getListOfDlcForGame(game_pk);
+
+        return dlcs;
+    }
 }
--- src/main/java/gameDB/gameDB/services/GameService.java
+++ src/main/java/gameDB/gameDB/services/GameService.java
@@ -1,4 +1,43 @@
 package gameDB.gameDB.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.HashMap;
+
+@Service
 public class GameService {
+    @Value("${rawg.api_key}")
+    private String api_key;
+    @Value("${rawg.base_path}")
+    private String base_path;
+
+    WebClient webClient = WebClient.create("https://api.rawg.io/api/");
+
+    public HashMap<String, Object> getAllGames() {
+
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder.path("games")
+                        .queryParam("key",api_key)
+                        .build()
+                )
+                .retrieve()
+                .bodyToMono(HashMap.class)
+                .block();
+    }
+
+    public HashMap<String,Object> getListOfDlcForGame(int game){
+
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder.path("games/"+game+"/additions")
+                        .queryParam("key",api_key)
+                        .build()
+                )
+                .retrieve()
+                .bodyToMono(HashMap.class)
+                .block();
+    }
+
 }
